302848ea42fce53068adbef73ea6090b00077f18
Evan MacDonald <113189803+Evan-Prime@users.noreply.github.com>
2023-02-21 08:57:56 -0400

    Initial commit

---
+++ StateMachine/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("State Machine");
+            Console.WriteLine("");
+            Console.WriteLine();
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("Press any button to exit.");
+            Console.ReadKey(true);
+        }
+    }
+}
