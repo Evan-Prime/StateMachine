9d9050154c66dba415b45d1a89b8d2709591ea49
Evan MacDonald <113189803+Evan-Prime@users.noreply.github.com>
2023-02-21 09:20:31 -0400

    Added state machine

--- StateMachine/Program.cs
+++ StateMachine/Program.cs
@@ -8,17 +8,64 @@ namespace StateMachine
 {
     internal class Program
     {
+        enum State
+        {
+            patrolling,
+            chasing,
+            searching,
+            attacking,
+            retreating
+        }
+
+        static State state;
+
         static void Main(string[] args)
         {
             Console.WriteLine("State Machine");
             Console.WriteLine("");
             Console.WriteLine();
 
-
+            state = State.patrolling;
+            EnemyUpdate();
+            EnemyUpdate();
+            EnemyUpdate();
+            EnemyUpdate();
+            EnemyUpdate();
 
             Console.WriteLine();
             Console.WriteLine("Press any button to exit.");
             Console.ReadKey(true);
+        }
+
+        static void EnemyUpdate()
+        {
+            switch (state)
+            {
+                case State.patrolling:
+                    Console.WriteLine("Patrolling... and potentially switching to chasing.");
+                    state = State.chasing;
+                    break;
+
+                case State.chasing:
+                    Console.WriteLine("Chasing... and potentially switching to attacking.");
+                    state = State.attacking;
+                    break;
+
+                case State.searching:
+                    Console.WriteLine("Searching... and potentially switching to chasing.");
+                    state = State.chasing;
+                    break;
+
+                case State.attacking:
+                    Console.WriteLine("Attacking... and potentially switching to retreating.");
+                    state = State.retreating;
+                    break;
+
+                case State.retreating:
+                    Console.WriteLine("Retreating... and potentially switching to patrolling.");
+                    state = State.patrolling;
+                    break;
+            }
         }
     }
 }
